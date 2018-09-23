ddc034fb570f07f871edb54d4c4db550fc58f5be
Ronny <rkjoloymo@gmail.com>
2018-09-23 21:31:47 +0200

    looks

--- links.sh
+++ links.sh
@@ -11,11 +11,11 @@ for file in ${folowing[*]};do
     printf "\nProsessing ${file}....\n"
     [ -e ${HOME}/.${file}.bak ] && \
         rm -r ${HOME}/.${file}.bak && \
-        echo "removing old ${HOME}/.${file}.bak"
+        echo "  --removing old ${HOME}/.${file}.bak"
     [ -e ${HOME}/.${file} ] && \
         mv ${HOME}/.${file} ${HOME}/.${file}.bak && \
-        echo "moving ${HOME}/.${file} to ${HOME}/.${file}.bak"
+        echo "  --moving ${HOME}/.${file} to ${HOME}/.${file}.bak"
     ln -s ${PWD}/${file} ${HOME}/.${file} && \
-        echo "linking ${PWD}/${file} with ${HOME}/.${file}"
+        echo "  --linking ${PWD}/${file} with ${HOME}/.${file}"
 done
 printf "\n\n"
